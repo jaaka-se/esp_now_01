133b48881e1eb0750149496fa13904779f39d652
jaaka <jaaka.se@gmail.com>
2022-01-10 23:19:42 +0100

    first version handling channel NOT 1
    with a disconnected WiFi_Mode_STA,tested from esp01 esp32wrom with broadcast

---
+++ MeshRC.h
@@ -0,0 +1,158 @@
+//
+// Author: Phong Vu
+//
+#ifndef __MESH_RC_H__
+#define __MESH_RC_H__
+
+#include <espnow.h>
+
+namespace MeshRC {
+#define MESH_RC_DEBUG_ALL_MSG false
+
+typedef std::function<void(void)> esp_rc_callback_t;
+typedef std::function<void(u8 *, u8)> esp_rc_data_callback_t;
+
+struct esp_rc_event_t {
+	String prefix;
+	esp_rc_callback_t callback;
+	esp_rc_data_callback_t callback2;
+} events[250];
+
+u8 buffer[250];
+u8 broadcast[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+u8 events_num = 0;
+u32 received;
+u32 ignored;
+u8 *sender = NULL;
+u32 sendTime;
+u16 duration;
+u8 *master = NULL;
+bool sending;
+// This is the crypto replase with some thing other !!!! must be the same for all ESP's
+u8 psk[] = {'1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f'};
+
+void setMaster(u8 *addr) {
+	if (esp_now_is_peer_exist(master))
+		esp_now_del_peer(master);
+	master = addr;
+	esp_now_add_peer(master, ESP_NOW_ROLE_COMBO, 1, psk, sizeof(psk));
+}
+void send(u8 *data, u8 size) {
+	sending = true;
+	sendTime = micros();
+	esp_now_send(master ? master : broadcast, data, size);
+}
+void send(String data) {
+	send((u8 *)data.c_str(), data.length());
+	Serial.printf(data.c_str());
+	Serial.printf("\n");
+}
+void log(String data, String message) {
+	
+	sending = false;
+}
+void send(String type, u8 *data, u8 size) {
+	memcpy(&buffer[0], (u8 *)type.c_str(), type.length());
+	memcpy(&buffer[type.length()], data, size);
+	send(buffer, type.length() + size);
+}
+void on(String prefix, esp_rc_callback_t callback) {
+	events[events_num++] = (esp_rc_event_t){prefix, callback, NULL};
+}
+void on(String prefix, esp_rc_data_callback_t callback) {
+	events[events_num++] = (esp_rc_event_t){prefix, NULL, callback};
+}
+void wait() {
+	while (MeshRC::sending) yield();  // Wait while sending
+}
+void delayMs(u32 time) {
+    u32 m = micros();
+    if(time){
+        u32 e = (m + time);
+//	u32 delayUntil = millis() + time;
+// this is not safe due to wraparound //	while (millis() < delayUntil) yield();
+        if(m > e){ //overflow
+            while(micros() > e){
+                yield();
+            }
+        }
+        while(micros() < e){
+            yield();
+        }
+    }	
+}
+bool equals(u8 *a, u8 *b, u8 size, u8 offset = 0) {
+	for (auto i = offset; i < offset + size; i++)
+		if (a[i] != b[i])
+			return false;
+	return true;
+}
+esp_now_send_cb_t sendHandler = [](u8 *addr, u8 err) {
+	sending = false;
+	duration = micros() - sendTime;
+
+  Serial.printf(" [ ");
+  for (auto i = 0; i < 5; i++) {
+    Serial.printf("%02X ", addr[i]);
+  }
+  Serial.printf("] status=%i \n",err);
+
+};
+esp_now_recv_cb_t recvHandler = [](u8 *addr, u8 *data, u8 size) {
+	static u8 offset, i;
+#ifdef MESH_RC_DEBUG_ALL_MSG
+	Serial.printf(":: MESH_RC RECV %u bytes: ", size);
+	for (i = 0; i < size; i++) {
+		Serial.write(data[i]);
+	}
+	Serial.printf(" [ ");
+	for (i = 0; i < size; i++) {
+		Serial.printf("%02X ", data[i]);
+	}
+	Serial.println("]");
+	if (addr != NULL ) {
+		Serial.printf(" Addr=");
+		for (auto a = 0; a < 6;a++) {
+			Serial.printf("%02X ", addr[a]);
+		}
+		Serial.println(" ");
+	}
+#endif
+	// Only receives from master if set
+	if (addr == NULL || master == NULL || equals(addr, master, 6)) {
+		received++;
+		sender = addr;
+		
+		for (i = 0; i < events_num; i++) {
+			offset = events[i].prefix.length();
+			if (equals(data, (u8 *)events[i].prefix.c_str(), offset)) {
+				if (events[i].callback) events[i].callback();
+				if (events[i].callback2) events[i].callback2(&data[offset], size - offset);
+			}
+		}
+	} else {
+		ignored++;
+	}
+};
+void begin(u8 channel) {
+	if (esp_now_init() == OK) {
+#ifdef MESH_RC_DEBUG_ALL_MSG
+	Serial.println(":: MESH_RC espnow OK: ");
+#endif
+		if (esp_now_is_peer_exist(broadcast)) {
+			esp_now_del_peer(broadcast);
+		}
+		esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+		esp_now_add_peer(broadcast, ESP_NOW_ROLE_COMBO, channel, 0, 0);
+		esp_now_register_send_cb(sendHandler);
+		esp_now_register_recv_cb(recvHandler);
+#ifdef MESH_RC_DEBUG_ALL_MSG
+	Serial.printf(":: MESH_RC espnow channel: %u \n",channel);
+#endif
+	} else {
+	Serial.println(":: MESH_RC espnow init failed: ");
+	}		
+}
+}  // namespace MeshRC
+
+#endif	//__MESH_RC_H__
---
+++ esp_now_01.ino
@@ -0,0 +1,99 @@
+/*
+
+*/
+#include "MeshRC.h"
+
+#include <ESP8266WiFi.h>
+#define CHANNEL 9
+#define DEBUG_ALL false
+#define FORSEDEEPSLEEP 25000
+const int buttonPin = 2;     // the number of the pushbutton pin
+
+ADC_MODE(ADC_VCC)  
+uint16_t batterylevel;  
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton statu
+int prevButtonState = 4711;
+unsigned long lastUpdate = millis();
+unsigned long currentMillis,delta1,delta2;
+void setup() {
+  WiFi.persistent(false);//don't store and read Wifi settings, this savesabout 300ms
+  Serial.begin(115200);
+
+  batterylevel = ESP.getVcc(); 
+
+  const char *ssid_not_used = "ESP001_sender_dummy";
+  const char *pwd_not_used ="dummy_not_used";
+  int ch = CHANNEL;
+ 
+#ifdef DEBUG_ALL
+  currentMillis = millis();
+  Serial.printf("WiFi.begin\n");
+#endif
+
+  WiFi.begin(ssid_not_used,pwd_not_used,ch,NULL,false);//Trick to initiate channel
+//without startinga connection when esp-now uses an other channel than 1(defaut for STA)
+
+#ifdef DEBUG_ALL
+  delta1 = millis()-currentMillis;
+  Serial.print(" 3 WiFi.channel=");
+  Serial.println(WiFi.channel());
+  currentMillis = millis();
+#endif
+
+  WiFi.disconnect(); //saves power ifonly esp-now is used
+  WiFi.softAPdisconnect(true);
+
+#ifdef DEBUG_ALL
+  delta2 = millis()-currentMillis; 
+  Serial.printf("WiFi.begin time = %u \n",delta1);
+  Serial.printf("WiFi.disconnect time = %u \n",delta2);
+#endif
+  Serial.print("WiFi.macAddress=");
+  Serial.println(WiFi.macAddress());
+  
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+
+//Channel must be the same for receiver and sender
+//it should be the same as youre WiFi-network if reciver
+//the receiver shall forvard to HA
+  MeshRC::begin(CHANNEL);
+
+  MeshRC::send("Remote esp started "+WiFi.macAddress());
+  String s = String(batterylevel);
+  MeshRC::send("Voltage: "+ s);
+}    
+
+
+void loop() {
+   currentMillis = millis();
+ // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (prevButtonState != buttonState) {
+    prevButtonState = buttonState;
+    if (buttonState == HIGH) {
+      MeshRC::send("Button activeted at ");
+    } else {
+      MeshRC::send("Button deactiveted at ");
+    }
+  }
+  
+  if ( currentMillis > (lastUpdate + 5000) ){
+    MeshRC::send("still running " + String(currentMillis));
+    lastUpdate = currentMillis;
+    batterylevel = ESP.getVcc(); 
+    MeshRC::send("Voltage: "+ String(batterylevel));
+  }
+  
+  if ( currentMillis > FORSEDEEPSLEEP ){
+    batterylevel = ESP.getVcc(); 
+    String s = String(batterylevel);
+    MeshRC::send("Voltage: "+ s);
+    MeshRC::send("Going to sleep ");
+    ESP.deepSleep(0);
+  }
+}
